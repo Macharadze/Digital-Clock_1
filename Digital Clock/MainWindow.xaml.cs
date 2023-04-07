2287a93cb2f0758ae8e48a9a469a230740f55e53
Macharadze <macharadze.giorgi@kiu.edu.ge>
2023-04-07 18:01:14 +0400

    Add project files.

---
+++ Digital Clock/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+using System.Timers;
+
+namespace Digital_Clock
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        static int i = 0;
+        public MainWindow()
+        {
+            InitializeComponent();
+             var timer =new System.Timers.Timer();
+            timer.Interval = 500;
+            timer.Elapsed += onTime; 
+            timer.Start();
+
+        }
+        private void timer_Main()
+        {
+            time_1.Text = DateTime.Now.ToString("HH:mm");
+            time_2.Text = DateTime.Now.ToString("ss");
+            date.Text = DateTime.Now.ToString("MMM dd yyyy");
+            day.Text = DateTime.Now.ToString("dddd");
+             
+        }
+        private void onTime(object sender,ElapsedEventArgs e)
+        {
+            i++;
+            Console.WriteLine();
+            lbl.Dispatcher.Invoke(() =>
+            {
+                timer_Main();
+            });   
+        }
+
+      
+    }
+}
